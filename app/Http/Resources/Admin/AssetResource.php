6e5682dcdb83d936fe214cd9bdfa999276c02a55
Aaron Shames <aaron@1coin.io>
2021-06-29 18:32:40 -0400

    order change

--- app/Http/Resources/Admin/AssetResource.php
+++ app/Http/Resources/Admin/AssetResource.php
@@ -8,14 +8,14 @@ class AssetResource extends JsonResource
 {
     public function toArray($request)
     {
-        $logouri = "https://1cointokens,com/assets/" . $this->network->chainid . "/" .
-            $this->contract_address . "/logo.png";
+        //   $logouri = "https://1cointokens,com/assets/" . $this->network->chainid . "/" .
+        //      $this->contract_address . "/logo.png";
         return [
-            "name" => $this->name,
             "chainId" => $this->network->chainid,
-            "symbol" => $this->symbol,
-            "decimals" => ($this->decimals) ? $this->decimals : 18,
             "address" => $this->contract_address,
+            "symbol" => $this->symbol,
+            "name" => $this->name,
+            "decimals" => ($this->decimals) ? $this->decimals : 18,
             "logoURI" => 'https://raw.githubusercontent.com/compound-finance/token-list/master/assets/ctoken_usdc.svg'
             //      "logoURI" => ($this->logoURI) ?                $this->logoURI
             //          : $logouri
